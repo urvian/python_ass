5543487319d69b8f3270abe16a00adaea0e7d12b
urvian <137489642+urvian@users.noreply.github.com>
2023-06-30 15:35:19 +0530

    Update Ass_6.py

--- Ass_6.py
+++ Ass_6.py
@@ -1,29 +1,33 @@
 def ds(roll,name):
     ls=[]
     ls.append((roll,name))
+    print("The list is:",ls)
     t=(roll,name)
+    print("The tuple is:",t)
     s=set()
     s={(roll,name)}
+    print("The set is:",s)
     d={
         "roll":roll,
         "name":name
     }
+    print("The dictionary is:",d)
     ls[0] = (5,"Swati")
     t=(5,"swati")
     s.pop()
     s.add((5,"Swati"))
     d["roll"]=5
     d["name"]="Swati"
-    print("The list is:",ls)
-    print("The list is:",t)
-    print("The list is:",s)
-    print("The list is:",d)
-    del(ls)
-    del(t)
-    del(s)
-    del(d)
-    print(ls)
-    print(t)
-    print(s)
-    print(d)
+    print("The updated list is:",ls)
+    print("The updated tuple is:",t)
+    print("The updated set is:",s)
+    print("The updated dictionary is:",d)
+    # del(ls)
+    # del(t)
+    # del(s)
+    # del(d)
+    # print(ls)
+    # print(t)
+    # print(s)
+    # print(d)
 ds(12,"Urvi")
