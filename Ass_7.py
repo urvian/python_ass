c4694268b43ffdf8596a5dd620049f032040c4cc
urvian <137489642+urvian@users.noreply.github.com>
2023-07-03 12:21:37 +0530

    Ass_7.py

--- Ass_7.py
+++ Ass_7.py
@@ -1,18 +1,19 @@
-def fileHandle(file):
-    try:
-        z=open(file,"a+")
-        z.writelines(["Rollno:5\n","name:Urvi\n","class:TYIF"])
-        z.seek(0)
-        print(z.readlines())
-    except FileNotFoundError:
-        print("The exception is handled")
-    except FileExistsError:
-        print("The exception is handled")
-    except IOError:
-        print("The exception is handled")
-    except Exception:
-        print("The exception is handled")
-    finally:
-        print("This is the finally block")
-# fileHandle("Sample1.txt")
-fileHandle("src.txt")
+class File:   
+    def fileHandle(file="src.txt"):
+        try:
+            z=open(file,"a+")
+            z.writelines(["Rollno:5\n","name:Urvi\n","class:TYIF"])
+            z.seek(0)
+            print(z.readlines())
+        except FileNotFoundError:
+            print("The exception is handled")
+        except FileExistsError:
+            print("The exception is handled")
+        except IOError:
+            print("The exception is handled")
+        except Exception:
+            print("The exception is handled")
+        finally:
+            print("This is the finally block")
+    # fileHandle("Sample1.txt")
+    fileHandle("src.txt")
