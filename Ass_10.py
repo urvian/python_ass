b627e00e16933604955fbb01d85c0c2969bae70c
urvian <137489642+urvian@users.noreply.github.com>
2023-07-03 20:47:29 +0530

    Ass_10.py

--- Ass_10.py
+++ Ass_10.py
@@ -7,6 +7,6 @@ def display():
     s=e.get()
     print(s+"navigating to youtube")
     wb.open("https://www.youtube.com/results?search_query="+s)
-b=tk.Button(obj,text="Find",font=("Times New Roman",30),command=display)
+b=tk.Button(obj,text="Find",font=("Times New Roman",30),command=display,bg="purple",activebackground="blue")
 b.grid(row=1,column=0)
 obj.mainloop()
