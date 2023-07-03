069424ae06d032f8e1ed3eda9060911c0dfb0326
urvian <137489642+urvian@users.noreply.github.com>
2023-07-03 20:42:01 +0530

    Add files via upload

---
+++ Ass_10.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import webbrowser as wb
+obj=tk.Tk()
+e=tk.Entry(obj,width=30,font=("Times New Roman",30))
+e.grid()
+def display():
+    s=e.get()
+    print(s+"navigating to youtube")
+    wb.open("https://www.youtube.com/results?search_query="+s)
+b=tk.Button(obj,text="Find",font=("Times New Roman",30),command=display)
+b.grid(row=1,column=0)
+obj.mainloop()
